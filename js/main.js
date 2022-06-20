1bb9bb62d0bdea99e2d0a9623dfd5527590c0a89
Franco Fleitas <francofleitasguti@gmail.com>
2022-06-20 17:17:55 -0300

    añadiendo creacion de varias notas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,52 @@
-/*const formulario = document.getElementById("formulario")
+const formulario = document.getElementById("formulario")
 const listaTareas = document.getElementById("lista-tareas")
 const input = document.getElementById("input")
 const template = document.getElementById("template").content
+const fragment = document.createDocumentFragment()
+let tareas = {}
 
-let noteContent;
-let note = "Todavía no has creado una nota";
-let noteSaves = [] */
+formulario.addEventListener("submit", e => {
+    e.preventDefault()
 
+    setTarea(e)
+})
+
+function setTarea(e){
+    if(input.value.trim() === ""){
+        return
+    }
+    const tarea = {
+        id: Date.now(),
+        texto: input.value,
+        estado: false
+    }
+    tareas[tarea.id] = tarea
+    formulario.reset()
+    input.focus()
+    imprimirTareas()
+}
+
+function imprimirTareas() {
+    listaTareas.innerHTML = ""
+    Object.values(tareas).forEach(tarea => {
+        const clone = template.cloneNode(true)
+        clone.querySelector("p").textContent = tarea.texto
+        fragment.appendChild(clone)
+    })
+    listaTareas.appendChild(fragment)
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 document.getElementById("btn").onclick = añadirNota;
 parrafo = document.getElementById("parrafoNota")
 
@@ -22,58 +62,4 @@ function añadirNota() {
     else {
         alert("Ingrese un valor válido")
     }
-}
-
-
-
-
-
-
-
-/*
-window.onload = startFunction;
-
-function startFunction() {
-        let start = prompt("Bienvenido, quiere crear una nueva nota o ver las últimas creadas?\n Para crear una nueva nota ingrese 1\n Para ver las últimas notas ingrese 2")
-    if (parseInt(start) === 1) {
-        newNote();
-        startFunction();
-    }
-    else if (parseInt(start) === 2) {
-        for (let index = 0; index < noteSaves.length; index++) {
-            alert(noteSaves[index])
-        }
-        while (noteSaves = []) {
-            alert(note);
-            break;
-        }
-        startFunction();
-    }
-    else if(start === null){
-        return;
-    }
-    else {
-        alert("Ingrese un valor válido")
-        startFunction();
-    }
-}
-
-function newNote () {
-    noteTitle = prompt("Ingrese el título de la nota");
-    if (noteTitle === null){
-        return;
-    }
-    else if (noteTitle == "") {
-        alert("Ingrese algún título")
-    }
-    noteContent = prompt("Perfecto!, a continuación escriba el contenido de la nota")
-    if (noteContent === null) {
-        return;
-    }
-    while (noteTitle != "" && noteContent != "") {
-        note = `${(noteTitle)} \n\n ${noteContent}`;
-        alert("Su nota ha sido guardada con éxito: \n" + note)
-        noteSaves.push(note)
-        return;
-    }
-} */
+}*/
